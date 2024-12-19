e4873ae880984d08e5eed315b1be7ac8df6e67a9
Cieliocas <francielioevangelista@hotmail.com>
2024-12-19 03:00:28 -0300

    vue.js tests

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
 const nome = "cielio castro";
+const meuObj = {
+	nome: "cielio",
+	filmeFavorito: "matrix"
+}
+
+function dizOla() {
+	return `${nome} diz oi`;
+}
 </script>
 
-
-
 <template>
-	<h1>{{ nome }}</h1>
+	<h1>{{ dizOla() }}</h1>
 </template>
 
 <style scoped>
